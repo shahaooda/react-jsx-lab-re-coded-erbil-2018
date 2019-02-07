8e6493911e2a50ef842d90495854c78d29665c82
Shahad Bakoos <shahadbakoos@gmail.com>
2019-02-07 14:24:00 +0000

    Automatically backed up by Learn

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import RegistrationForm from './components/RegistrationForm';
+import Webpage from './components/Webpage';
+
+ReactDOM.render(
+  (
+    <div>
+      <RegistrationForm />
+      <Webpage />
+    </div>
+  ),
+  document.getElementById('main')
+);
